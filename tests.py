1775799faeda92c14e82ce75631ce308e27e360b
vsilent <jabberroid@gmail.com>
2019-05-15 16:40:53 +0300

    tests added

--- tests.py
+++ tests.py
@@ -34,9 +34,9 @@ assert php.status == 'running'
 php_conf = php.exec_run("php-fpm7.0 -t")
 # print(php_conf.output.decode())
 assert 'configuration file /etc/php/7.0/fpm/php-fpm.conf test is successful' in php_conf.output.decode()
-php_proc = php.exec_run("ps aux |grep php-fpm")
-# print(php_proc.output.decode())
-# assert 'php-fpm: master process (/usr/local/etc/php-fpm.conf)' in php_proc.output.decode()
+php_proc = php.exec_run("sh -c 'ps aux |grep php-fpm'")
+print(php_proc.output.decode())
+assert 'php-fpm: master process (/usr/local/etc/php-fpm.conf)' in php_proc.output.decode()
 
 assert 'fpm is running, pid' in php.logs()
 # response = requests.get("http://localhost")
