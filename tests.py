0ba3c93f028d1eaefa483ca2d3c9640e4075db20
vsilent <jabberroid@gmail.com>
2019-05-27 15:07:18 +0300

    test http response

--- tests.py
+++ tests.py
@@ -39,15 +39,20 @@ assert 'configuration file /etc/php/7.0/fpm/php-fpm.conf test is successful' in 
 php_proc = php.exec_run("sh -c 'ps aux |grep php-fpm'")
 print(php_proc.output.decode())
 assert 'php-fpm: master process (/etc/php/7.0/fpm/php-fpm.conf)' in php_proc.output.decode()
-
+assert 'wordpre+' in php_proc.output.decode()
+assert 'php-fpm: pool www' in php_proc.output.decode()
 assert 'fpm is running, pid' in php.logs()
-# response = requests.get("http://localhost")
-# assert response.status_code == 200
 
 mysql = client.containers.get('wpdb')
 assert mysql.status == 'running'
 mycnf = mysql.exec_run("/usr/sbin/mysqld --verbose  --help")
-assert '/usr/sbin/mysqld  Ver 5.7.26' in mycnf.output.decode()
+# print(mycnf.output.decode())
+print(mysql.logs())
 mysql_log = mysql.logs()
+assert "Version: '5.7.25'" in mysql_log
 assert "mysqld: ready for connections" in mysql_log.decode()
-print(mysql_log.decode())
+
+response = requests.get("http://localhost")
+assert response.status_code == 200
+print(response.text)
+assert 'Welcome to the famous five-minute WordPress installation process!' in response.text
